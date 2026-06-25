2d2752c22644d8ba81d43f2603ca60d8b135f146
Stefano Maffeis <lesbass@gmail.com>
2026-06-25 07:16:40 +0000

    fix: add xmlns:image namespace to sitemap.xml to resolve XML parsing errors
    The sitemap generates <image:image> elements but never declared the
    xmlns:image namespace on the <urlset> root element. This caused XML
    parsing errors on lines 283-286 of the sitemap. Added:
      xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -52,7 +52,7 @@ export async function GET(context) {
   ].join('\n');
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 ${urls}
 </urlset>`;
   return new Response(xml, {
